0b784a7f671e08a82f3a920e9fa29de21a88f2f8
seumsims <seumworldinbangladesh@gmail.com>
2022-12-09 00:10:12 +0600

    master imgenopenai add some quick start server code and take an env file done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(1,2,3)
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const coms = express();
+
+coms.listen(port, () => console.log(`Server started on port ${port}`));
